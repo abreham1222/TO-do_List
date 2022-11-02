a412c86c79d3d44a80b960bb0cc3f433d7ebff4d
abreham1222 <abreham1222@gmail.com>
2022-11-02 22:43:16 +0300

    Add all webpack files

--- webpack.config.js
+++ webpack.config.js
@@ -9,10 +9,30 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Todo-List',
-
+      template: './src/index.html',
+      hash: true,
     })
-  ]
+  ],
+   module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
 }
