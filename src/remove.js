f673d17d96ade6476d77e6b132862e1152485432
abreham1222 <abreham1222@gmail.com>
2022-11-04 21:47:43 +0300

    Add fix linters

--- src/remove.js
+++ src/remove.js
@@ -6,9 +6,6 @@ const clearTask = (index, list) => {
   document.querySelector(`#task-${index}`).parentNode.parentNode.remove();
   localStorage.setItem('tasks', JSON.stringify(list));
   if (list.length === 0) return;
-
-  for (let i = index - 1; i < list.length; i += 1) {
-  }
 };
 
 export default clearTask;
