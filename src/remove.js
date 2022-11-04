881620bff4ba1badb78203d6a3aee239774ae747
abreham1222 <abreham1222@gmail.com>
2022-11-04 21:35:08 +0300

    Add clear code

--- src/remove.js
+++ src/remove.js
@@ -8,14 +8,6 @@ const clearTask = (index, list) => {
   if (list.length === 0) return;
 
   for (let i = index - 1; i < list.length; i += 1) {
-    /*
-    let ind = i + 2;
-   let nextCheck = document.querySelector(`#task-${ind}`)
-    nextCheck.attributes.id = `task-${i + 1}`;
-    nextCheck.value -= 1;
-    list[i].index -= 1;
-    localStorage.setItem('tasks', JSON.stringify(list));
-    */
   }
 };
 
